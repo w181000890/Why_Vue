e83842e30e60a6ba398a5b90cd6075cc1f4a8d55
wm <164078906@qq.com>
2023-04-03 07:55:49 +0800

    1

--- 03_learn_component/src/02_组件通信-父传子/App.vue
+++ 03_learn_component/src/02_组件通信-父传子/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>header</div>
+  <div>
+    <ShowInfos name="why" age ="12" :height="1.88"></ShowInfos>
+      <ShowInfos name="wm" age ="18" :height="1.89"></ShowInfos>
+  </div>
 </template>
 
 <script>
+import ShowInfos from './ShowInfos.vue';
 export default {
+  name:'App',
+  components:{
+    ShowInfos
+  }
 
 }
 </script>
---
+++ 03_learn_component/src/02_组件通信-父传子/ShowInfos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>姓名：{{name}}</h2>
+    <h2>年龄：{{age}}</h2>
+    <h2>身高：{{height}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"ShowInfos",
+  props:['name','age','height']
+
+}
+</script>
+
+<style>
+
+</style>
