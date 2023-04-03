a639380087bb81e8949f09601d8b4bf40412bba0
meng <s81000890@hnu.edu.cn>
2023-04-03 22:57:24 +0800

    1

---
+++ 03_learn_component/src/04_组件通信_案例练习/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <ListItem :productItem="['衣服','鞋子','裤子','帽子']"></ListItem>
+  </div>
+</template>
+
+<script>
+import ListItem from './ListItem.vue';
+export default {
+  components:{
+    ListItem
+  }
+}
+</script>
+
+<style scoped>
+.bar{
+  display: flex;
+  justify-content: space-around;
+}
+
+</style>
---
+++ 03_learn_component/src/04_组件通信_案例练习/ListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="bar" v-for="(item,index) in productItem" :key='item'>{{ item }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    productItem:{
+      type:Array,
+      default:()=>[]
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  .bar{
+  display: flex;
+  justify-content: space-around;
+  height: 44px;
+  line-height: 44px;
+
+}
+</style>
