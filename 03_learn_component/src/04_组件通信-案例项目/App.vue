d92147055352a51f96a9744949d1bb889918a735
meng <s81000890@hnu.edu.cn>
2023-04-04 22:08:13 +0800

    1

---
+++ 03_learn_component/src/04_组件通信-案例项目/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <ProductItem :proList = "['裤子','鞋子','帽子']" :itemhtml="['裤子列表','鞋子列表','帽子列表']"></ProductItem>
+  </div>
+</template>
+
+<script>
+  import ProductItem from './ProductItem.vue';
+export default {
+  components:{
+    ProductItem
+  }
+
+}
+</script>
+
+<style>
+
+</style>
