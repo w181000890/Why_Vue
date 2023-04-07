b603a61c2cdd0349de57c84da96868365321b335
meng <s81000890@hnu.edu.cn>
2023-04-07 21:18:29 +0800

    1

--- 03_learn_component/src/main.js
+++ 03_learn_component/src/main.js
@@ -6,6 +6,7 @@ import { createApp } from 'vue'
 // import App from './04_组件通信_校验/App.vue'
 // import App from './05_组件插槽/App.vue'
 // import App from './06_组件插槽_具名插槽/App.vue'
-import App from './07_组件通信_具名插槽/App.vue'
+// import App from './07_组件通信_具名插槽/App.vue'
+import App from './08_Provide和Inject/App.vue'
 
 createApp(App).mount('#app')
