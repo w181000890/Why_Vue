e7f5f7888b58b2d9eff5f4c8f9dd4c3e4bfefbf3
wm <164078906@qq.com>
2023-04-14 08:38:00 +0800

    1

--- 03_learn_component/src/11_ref获取元素组件/App.vue
+++ 03_learn_component/src/11_ref获取元素组件/App.vue
@@ -1,10 +1,31 @@
 <template>
-    
+    <div class="app">
+      <h2 ref="message">{{ message }}</h2>
+      <button @click="message='hello vue'" ref="btn">改变message</button>
+      <button @click="getRef">获取ref</button>
+      <Banner ref="banner"></Banner>
+    </div>
 </template>
 
 <script>
+import Banner from './Banner.vue';
 export default {
-
+  components:{
+    Banner
+  },
+  data(){
+    return{
+      message:"HEllo World"
+    }
+  },
+  methods:{
+    getRef(){
+      console.log(this.$refs.message)
+      console.log(this.$refs.banner)
+      console.log(this.$refs.banner.changeinfo)
+      console.log(this.$refs.banner.$el.nextSibling)
+    }
+  }
 }
 </script>
 
--- 03_learn_component/src/11_ref获取元素组件/Banner.vue
+++ 03_learn_component/src/11_ref获取元素组件/Banner.vue
@@ -1,10 +1,15 @@
 <template>
-  
+  <div class="banner">我是banner元素</div>
+  <button>点击</button>
 </template>
 
 <script>
 export default {
-
+  methods:{
+    changeinfo(){
+      console.log("changeinfo")
+    }
+  }
 }
 </script>
 
