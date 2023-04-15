6599489b94aa308c9989138f8cabde679aee3c56
meng <s81000890@hnu.edu.cn>
2023-04-15 16:55:22 +0800

    1

---
+++ 03_learn_component/src/12_动态组件的使用/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    about组件-{{ name }}--{{ age }}
+    <button @click="getmessage" >点击输出</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    name:{
+      type:String,
+      default:""
+    },
+    age:{
+      type:Number,
+      default:0
+    }
+  },
+  emits:["changeinfo"],
+  methods:{
+    getmessage(){
+      this.$emit("changeinfo","你好 vue")
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 03_learn_component/src/12_动态组件的使用/views/Category.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    category组件
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
