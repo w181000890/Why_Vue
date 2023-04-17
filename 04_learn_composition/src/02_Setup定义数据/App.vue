03fa81e4f6f6cdb2962fb46577e3acc2635e1814
wm <164078906@qq.com>
2023-04-17 16:02:39 +0800

    1

--- 04_learn_composition/src/02_Setup定义数据/App.vue
+++ 04_learn_composition/src/02_Setup定义数据/App.vue
@@ -6,6 +6,12 @@
     <h2>姓名：{{account.name}}</h2>
     <h2>密码：{{account.password}}</h2>
     <button @click="changeAccount">改变账户名</button>
+    <hr>
+    <h2>counter:{{ counter }}</h2>
+    <button @click="counter++">+1</button>
+    <hr>
+    <h2>info.counter:{{ info.counter }}</h2>
+    <button @click="info.counter.value++">+1</button>
   </div>
 </template>
 
@@ -14,6 +20,13 @@ import {ref,reactive} from 'vue'
 export default {
   setup(){
     let message = ref("hello vue")
+    let counter = ref(0)
+    let info ={
+      counter
+    }
+    let changeinfo = ()=>{
+      counter.value++
+    }
     let account = reactive({
       name:'wm',
       password:'1234'
@@ -31,7 +44,10 @@ export default {
       message,
       changeMessage,
       account,
-      changeAccount
+      changeAccount,
+      counter,
+      info
+
     }
   }
 }
