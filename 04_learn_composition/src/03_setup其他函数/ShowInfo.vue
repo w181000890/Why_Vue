68008e2ef5cc6ff0a80602ea8d7100a0f0a246a1
wm <164078906@qq.com>
2023-04-18 13:46:18 +0800

    1

---
+++ 04_learn_composition/src/03_setup其他函数/ShowInfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="showInfo">
+    <h2>showInfo:{{ info }}</h2>
+    <button @click="infochangeName">改变name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"showInfo",
+  emits:["infochangeName"],
+  props:{
+    info:{
+      type:Object,
+      default:()=>({})
+    }
+  },
+  setup(prop,content){
+    function infochangeName(){
+      console.log("111")
+      content.emit("infochangeName","koke")
+    }
+    return {
+      infochangeName
+    }
+    
+  }
+  
+
+}
+</script>
+
+<style>
+
+</style>
