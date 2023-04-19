f64e32090b75b1b682e85c1ae6f565ad00922bc0
wm <164078906@qq.com>
2023-04-19 18:11:06 +0800

    1

--- 04_learn_composition/src/04_Setup中的torefs函数/App.vue
+++ 04_learn_composition/src/04_Setup中的torefs函数/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="app">
-
+  <h2>app:{{ info }}</h2>
+  <h2>{{ name }}</h2>
+  <button @click="name = 'wm'">改变名字</button>
+  <button @click="name = 'wm'">改变名字</button>
   </div>
 </template>
 
@@ -9,7 +12,17 @@ import {reactive,toRefs} from 'vue'
 export default {
 
   setup(props,context){
-    
+    let info= reactive({
+      name:'why',
+      age:18,
+      height:1.88
+    })
+    const {name,age} = toRefs(info)
+    const height = toRef(info)
+
+    return {
+      info,name,age,height
+    }
   }
 
 }
