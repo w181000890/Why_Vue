680b23010ec6590af652d02dc85e3dcc52ae99ef
wm <164078906@qq.com>
2023-04-20 06:29:02 +0800

    1

--- 04_learn_composition/src/05_Setup中computed/App.vue
+++ 04_learn_composition/src/05_Setup中computed/App.vue
@@ -1,5 +1,9 @@
 <template>
-  
+  <div class="app">
+    <h2>名字：{{name.firstname}}{{ name.lastname }}</h2>
+    <h2>分数：{{scoreflag}}</h2>
+    <button @click="changename">点击改变名字</button>
+  </div>
 </template>
 
 <script>
@@ -12,13 +16,27 @@ export default {
     })
     const score = ref(20)
     let scoreflag = computed(()=>{
-      get
+       return score.value>60?"及格":'不及格'
     })
     let fullname = computed({
+      get:function(){
+        return name.firstname+name.lastname
+      },
+      set:function(newvalue){
+        let tempName = newvalue.split(" ")
+        name.firstname = tempName[0]
+        name.lastname = tempName[1]
+      }
 
   })
+  function changename(){
+    fullname.value = 'zhang shan'
+  }
   return{
+    name,
     fullname,
+    scoreflag,
+    changename
   }
   }
   
