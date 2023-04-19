9f8cd5a79202ff555e3c3eb9aa7c57f91b3262f2
wm <164078906@qq.com>
2023-04-19 11:57:04 +0800

    1

---
+++ 04_learn_composition/src/04_Setup中的torefs函数/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+
+  </div>
+</template>
+
+<script>
+import {reactive,toRefs} from 'vue'
+export default {
+
+  setup(props,context){
+    
+  }
+
+}
+</script>
+
+<style>
+
+</style>
