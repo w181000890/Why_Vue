6b874504386e82a130f0dd5f6f8e0aa7f0fde37a
meng <s81000890@hnu.edu.cn>
2023-04-19 21:58:46 +0800

    1

--- 04_learn_composition/src/05_Setup中computed/App.vue
+++ 04_learn_composition/src/05_Setup中computed/App.vue
@@ -3,7 +3,27 @@
 </template>
 
 <script>
+import {reactive,computed,ref} from 'vue'
 export default {
+  setup(){
+    const name = reactive({
+      firstname:"wang",
+      lastname:"meng"
+    })
+    const score = ref(20)
+    let scoreflag = computed(()=>{
+      get
+    })
+    let fullname = computed({
+
+  })
+  return{
+    fullname,
+  }
+  }
+  
+
+
 
 }
 </script>
