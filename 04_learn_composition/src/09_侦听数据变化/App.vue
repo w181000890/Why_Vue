b4f60ec519d892966f19bec0476bc44b0d5d6c31
wm <164078906@qq.com>
2023-04-20 20:52:58 +0800

    1

--- 04_learn_composition/src/09_侦听数据变化/App.vue
+++ 04_learn_composition/src/09_侦听数据变化/App.vue
@@ -1,10 +1,31 @@
 <template>
-  
+  <div class="app">
+    <h2>姓名是：{{name}}</h2>
+    <h2>当前计数为：{{counter}}</h2>
+    <button @click="counter++">点击+1</button>
+  </div>
 </template>
 
 <script>
+import {ref,watch,watchEffect} from 'vue'
 export default {
-
+  setup(){
+    let counter = ref(0)
+    let name = ref('why')
+    // watch(counter,(newValue,oldValue)=>{
+    //   console.log(newValue,oldValue)
+    // })
+    let stopWarch = watchEffect(()=>{
+      console.log('--------',counter.value)
+      if(counter.value>10){
+        stopWarch()
+      }
+    })
+    return {
+      counter,
+      name
+    }
+  }
 }
 </script>
 
--- 04_learn_composition/src/main.js
+++ 04_learn_composition/src/main.js
@@ -8,6 +8,7 @@ import { createApp } from 'vue'
 // import App from './06_setup中ref引入元素/App.vue'
 // import App from './07_生命周期函数/App.vue'
 // import App from './08_Setup-Provide-Inject/App.vue'
-import App from './09_侦听数据变化/App.vue'
+// import App from './09_侦听数据变化/App.vue'
+import App from './09_侦听数据变化/AppWatch.vue'
 
 createApp(App).mount('#app') 
