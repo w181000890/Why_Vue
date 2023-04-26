d6661529720407af74d3ae5f4935df28ee0bcd31
meng <s81000890@hnu.edu.cn>
2023-04-26 21:55:00 +0800

    1

--- 04_learn_composition/src/09_侦听数据变化/AppWatch.vue
+++ 04_learn_composition/src/09_侦听数据变化/AppWatch.vue
@@ -1,12 +1,47 @@
 <template>
   <div class="app">
     <h2>App</h2>
+    <button @click="message='你好，李银河'">修改message</button>
+    <button @click="info.frient.name='kames'">修改Info</button>
   </div>
 </template>
 
 <script>
+import {reactive,ref,watch} from 'vue'
 export default {
+  setup(){
+    //定义数据
+    const message = ref("hello world")
+    const info = reactive({
+      name:'why',
+      age:18,
+      frient:{
+        name:'kobe'
+      }
+    })
+    
+    //监听数据
 
+    watch(message,(newvalue,oldvalue)=>{
+      console.log(newvalue,oldvalue)
+    })
+    watch(info,(newValue,oldValue)=>{
+      console.log(newValue,oldValue)
+      console.log(newValue===oldValue)
+    },{
+      immediate:true
+    })
+    watch(()=>({...info}),(newVlaue,oldValue)=>{
+      console.log(newVlaue,oldValue)
+    },{
+      immediate:true,
+      deep:true
+    })
+    return{
+      message,
+      info
+    }
+  }
 }
 </script>
 
