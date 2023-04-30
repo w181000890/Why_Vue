f3beaaf3bfca9b4bff1087f28f1b27ddade63a4a
meng <s81000890@hnu.edu.cn>
2023-04-30 11:49:40 +0800

    1

--- 05_roomsource_demo/src/App.vue
+++ 05_roomsource_demo/src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <area-header
+    :title="highScore.title"
+    :subtitle="highScore.subtitle"></area-header>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup >
+import highScore from './data/high_score.json'
+import AreaHeader from './components/AreaHeader.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less" scoped>
+  .app{
+    width: 1024px;
+    padding: 20px;
+    margin:0 auto;
+  }
 </style>
---
+++ 05_roomsource_demo/src/components/AreaHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="area-header">
+    <h3 class="title">{{ title }}</h3>
+    <div class="subtitle">{{ subtitle }}</div>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    title:{
+      type:String,
+      default:"默认标题"
+    },
+    subtitle:{
+      type:String,
+      default:"默认标题"
+    }
+  })
+</script>
+
+<style lang="less" scoped>
+  .areaheader{
+    height: 84px;
+    .title{
+      font-size: 22px;
+    }
+    .subtitle{
+      font-size: 16px;
+      margin-top: 12px;
+    }
+  }
+</style>
