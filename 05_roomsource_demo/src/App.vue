c5262eccc96711e0e3d257cba5b699ce7d1453ea
meng <s81000890@hnu.edu.cn>
2023-05-01 07:39:46 +0800

    1

--- 05_roomsource_demo/src/App.vue
+++ 05_roomsource_demo/src/App.vue
@@ -3,13 +3,24 @@
     <area-header
     :title="highScore.title"
     :subtitle="highScore.subtitle"></area-header>
+    <!-- 图片列表 -->
+  <div class="item-list">
+    <template v-for="item in highScore.list" :key="item.id">
+      <div class="item-inner">
+        <div class="cover">
+          <img src="item.picture_url" alt="">
+        </div>
+      </div>
+    </template>
+    </div>
   </div>
+  
 </template>
 
 <script setup >
 import highScore from './data/high_score.json'
 import AreaHeader from './components/AreaHeader.vue'
-
+console.log(highScore)
 
 </script>
 
