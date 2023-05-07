606fe9f65dc1c3f0ba82848f391df70e411f8ee2
meng <s81000890@hnu.edu.cn>
2023-05-07 22:33:07 +0800

    1

---
+++ 07_learn_vuex/src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from "vuex";
+
+
+
+const store  = createStore({
+  state:()=>({
+    counter:100
+  })
+})
+
+export default store
---
+++ 07_learn_vuex/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+  <h2>HOme组件计数器：{{ $store.state.counter }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
