5fb608f1355fb7c31d0d8258c24e5d29637c7687
meng <s81000890@hnu.edu.cn>
2023-05-14 11:21:13 +0800

    1

--- 09_learn_axios/src/main.js
+++ 09_learn_axios/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+axios.request({
+  url:"http://123.207.32.32:8000/home/multidata"
+}).then(res=>{
+  console.log("res",res.data)
+})
