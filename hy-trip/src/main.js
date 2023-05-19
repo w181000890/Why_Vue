d2ef882a0d95480b72276361b31dcd9930d54807
wm <164078906@qq.com>
2023-05-19 18:32:11 +0800

    1

--- hy-trip/src/main.js
+++ hy-trip/src/main.js
@@ -2,6 +2,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import router from './router'
+
 import 'normalize.css'
 import './assets/css/index.css'
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
