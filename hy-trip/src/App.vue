5684caa05e39d8e96ad93c42f68dbe69400effcd
meng <s81000890@hnu.edu.cn>
2023-05-21 17:26:30 +0800

    1

--- hy-trip/src/App.vue
+++ hy-trip/src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="app">
     app 
-    <router-view></router-view>
-    <router-link to="/home">首页</router-link>
-    <router-link to="/favor">收藏</router-link>
-    <router-link to="/order">订单</router-link>
-    <router-link to="/message">消息</router-link>
+   <tab-bar></tab-bar>
   </div>
 </template>
 
 <script setup>
+import TabBar from '@/components/tarb-bar/tab-bar.vue'
 </script>
 
 
---
+++ hy-trip/src/components/tarb-bar/tab-bar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tab-bar">
+    <div class="tab-bar-item">首页</div>
+    <div class="tab-bar-item">收藏</div>
+    <div class="tab-bar-item">订单</div>
+    <div class="tab-bar-item">消息</div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+.tab-bar{
+  position:fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 50px;
+  display: flex;
+  border-top:1px solid #f3f3f3;
+  .tab-bar-item{
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
