dd0bcdf00ce5432abab417060e6408a90956dd63
wm <164078906@qq.com>
2023-05-23 08:17:25 +0800

    1

--- hy-trip/src/components/tarb-bar/tab-bar.vue
+++ hy-trip/src/components/tarb-bar/tab-bar.vue
@@ -12,10 +12,10 @@
     </div>
    </template> -->
    <van-tabbar v-model="active">
-  <van-tabbar-item icon="home-o">标签</van-tabbar-item>
-  <van-tabbar-item icon="search">标签</van-tabbar-item>
-  <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
-  <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+  <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+  <van-tabbar-item icon="search">收藏</van-tabbar-item>
+  <van-tabbar-item icon="friends-o">订单</van-tabbar-item>
+  <van-tabbar-item icon="setting-o">消息</van-tabbar-item>
 </van-tabbar>
 
    </div>
