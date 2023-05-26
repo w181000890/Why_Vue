28fa13248e003ba556cdac41b9717c3b5c291aa0
wm <164078906@qq.com>
2023-05-26 08:02:39 +0800

    1

--- hy-trip/src/views/city/city.vue
+++ hy-trip/src/views/city/city.vue
@@ -1,17 +1,39 @@
 <template>
   <!-- <div class="city top-page"> -->
   <div class="city">
-    <h2>City</h2>
-    <template v-for="item of 100" :key="item">
-        <div>哈哈哈哈哈{{ item }}</div>
+    <van-search
+      v-model="searchValue"
+      placeholder="城市/区域/位置"
+      show-action
+      shape="round"
+      @cancel="onBackHome"
+    />
+    <van-tabs v-model:active="tabActive" color="#ff9854">
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+    </van-tabs>
+
+    <template v-for="item in 100" :key="item">
+      <div>哈哈哈哈哈{{ item }}</div>
     </template>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
+const route = useRouter();
+const searchValue = ref("");
+const onBackHome = () => {
+  route.push("/home");
+};
+
+const tabActive = ref();
 </script>
 
 <style lang="less" scoped>
-
+.city {
+  --van-tabs-line-height: 30px;
+}
 </style>
