e1990dea122a3435a85023a73f8e244878d6950d
meng <s81000890@hnu.edu.cn>
2023-06-11 10:48:45 +0800

    1

--- hy-trip/vite.config.js
+++ hy-trip/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,5 +18,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {                // ← ← ← ← ← ←
+    host: '0.0.0.0'    // ← 新增内容 ←
   }
 })
