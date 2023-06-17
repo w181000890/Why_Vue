f7924a330e228a8d3f57a2f11bd45fdcef67814c
meng <s81000890@hnu.edu.cn>
2023-06-17 14:27:47 +0800

    1

--- hy-trip/vite.config.js
+++ hy-trip/vite.config.js
@@ -7,7 +7,7 @@ import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({@1223 
   plugins: [
     vue(),
     Components({
