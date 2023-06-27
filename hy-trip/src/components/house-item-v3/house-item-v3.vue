e27d8b890dc512221b0bf4dd5d30058dd7b2fa6b
wm <164078906@qq.com>
2023-06-27 20:55:07 +0800

    1

---
+++ hy-trip/src/components/house-item-v3/house-item-v3.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home-item">
+    <div class="item-inner">
+      <div class="cover">
+        <img src="" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  itemData:{
+    type:object,
+    default:()=>({})
+  }
+})
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- hy-trip/src/views/home/cpns/home-content.vue
+++ hy-trip/src/views/home/cpns/home-content.vue
@@ -12,6 +12,9 @@
 <script setup>
 import useHomeStore from "@/stores/modules/home";
 import { storeToRefs } from "pinia";
+import HouseItemV9 from "@/components/house-item-v9/house-item-v9.vue";
+import HouseItemV3 from "@/components/house-item-v3/house-item-v3.vue";
+
 
 const homeStore = useHomeStore();
 homeStore.fetchHouselistData();
